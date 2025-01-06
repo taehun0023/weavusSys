32aac0f10cf57b23ffa3a20b25a86079c29a324c
Your Name <you@example.com>
2025-01-06 15:22:55 +0900

    직원수정 및 등록부분 로직 수정

--- src/main/java/com/weavusys/hrd/repo/AccrualRepository.java
+++ src/main/java/com/weavusys/hrd/repo/AccrualRepository.java
@@ -3,9 +3,7 @@ package com.weavusys.hrd.repo;
 import com.weavusys.hrd.entity.Accrual;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AccrualRepository extends JpaRepository<Accrual, Long> {
-    Optional<Accrual> findByEmployeeId(String id);
+    Accrual findByEmployeeId(String id);
 }
 
